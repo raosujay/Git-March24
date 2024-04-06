3bf428b2b6da99e7e553186c1490ce386e7fa609
Sujay S <s.sujay@hotmail.com>
2024-04-07 00:16:30 +0530

    added comments

--- src/Main.java
+++ src/Main.java
@@ -2,5 +2,7 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello World");
         System.out.println("Addition of two numbers: " + Calculator.add(10, 20));
+
+        //lets add some comments here and save.
     }
 }
