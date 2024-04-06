496cb25924d7b31c4db2bc4f49fe5ed067c19019
Sujay S <s.sujay@hotmail.com>
2024-04-07 00:02:09 +0530

    added calculator class

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,6 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello World");
-        System.out.println("This is Java Programming");
+        System.out.println("Addition of two numbers: " + Calculator.add(10, 20));
     }
 }
